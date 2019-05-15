111c9b3d74f324ce1ccecda3b42b7973b077911d
juanmaperez <juanmaperezvar@gmail.com>
2019-05-15 21:23:07 +0100

    Query and Mutation

---
+++ src/components/LinkList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Link from './Link';
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+
+const FEED_QUERY = gql`
+  {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+      }
+    }
+  }`
+
+const LinkList = () => {
+
+  return (
+      <Query query={FEED_QUERY}>
+        {({loading, error, data}) => {
+          if (loading) return <div>fetching...</div>
+          if(error) return <div>Error...</div>
+          
+          const linksToRender = data.feed.links;
+
+          return (
+            linksToRender.map((link) => <Link key={link.id} link={link}/>)
+          )
+        }}
+        
+      </Query>
+    )
+}
+
+export default LinkList
