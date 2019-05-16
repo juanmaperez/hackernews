cebcb9959ac51c987cf2d8621c3126603134d659
juanmaperez <juanmaperezvar@gmail.com>
2019-05-16 09:22:55 +0100

    login implementation

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './../styles/App.css';
+
 import LinkList from './LinkList'
 import CreateLink from './CreateLink'
+import Login from './Login'
+
 import Header from './Header'
 import { Switch, Route } from 'react-router-dom'
 
@@ -13,6 +16,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={LinkList} />
           <Route exact path="/create" component={CreateLink} />
+          <Route exact path="/login" component={Login} />
         </Switch>
       </div>
     </div> 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import { Link as RouterLink } from 'react-router-dom'
 import { withRouter } from 'react-router'
+import { AUTH_TOKEN } from '../constants'
+
 
 class Header extends Component {
   render(){
+    const authToken = localStorage.getItem(AUTH_TOKEN)
+
     return(
       <div className="flex pa1 justify-between nowrap orange">
         <div className="flex pa1 justify-between nowrap orange">
@@ -11,10 +15,30 @@ class Header extends Component {
             <RouterLink to="/" className="ml1 no-underline black">
               new
             </RouterLink>
-            <div className="ml1">|</div>
-            <RouterLink to="/create" className="ml1 no-underline black">
-              submit
+            {authToken && (
+            <div className='flex flex-fixed'>  
+              <div className="ml1">|</div>
+              <RouterLink to="/create" className="ml1 no-underline black">
+                submit
+              </RouterLink>
+            </div> )}
+        </div>
+        <div className="flex flex-fixed">
+          { authToken ? (
+            <div
+              className="ml1 pointer black"
+              onClick={() => {
+                localStorage.removeItem(AUTH_TOKEN)
+                this.props.history.push(`/`)
+              }}
+            >
+              logout
+            </div>
+          ) : (
+            <RouterLink to="/login" className="ml1 no-underline black">
+              login
             </RouterLink>
+          )}
         </div>
       </div>
     )
