84b09309389856ac3a62972073c88b47d033caec
juanmaperez <juanmaperezvar@gmail.com>
2019-05-18 15:08:18 +0100

    app subscriptions

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from './Link';
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
@@ -25,9 +25,57 @@ export const FEED_QUERY = gql`
     }
   }`
 
-const LinkList = () => {
+  const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`
 
-  const _updateCacheAfterVote = (store, createVote, linkId) => {
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`
+
+class LinkList extends Component {
+
+  _updateCacheAfterVote = (store, createVote, linkId) => {
     const data = store.readQuery({ query: FEED_QUERY })
     const votedLink = data.feed.links.find( link => link.id === linkId)
     votedLink.votes = createVote.link.votes;
@@ -35,12 +83,56 @@ const LinkList = () => {
     store.writeQuery({ query: FEED_QUERY, data })
   }
 
-  return (
+  // You’re passing two arguments to subscribeToMore:
+  // document:    This represents the subscription query itself. In your case, 
+  //              the subscription will fire every time a new link is created.
+  // updateQuery: Similar to cache update prop, this function allows you to determine 
+  //              how the store should be updated with the information 
+  //              that was sent by the server after the event occurred. 
+  //              In fact, it follows exactly the same principle as a Redux reducer: 
+  //              It takes as arguments the previous state (of the query that subscribeToMore was called on) 
+  //              and the subscription data that’s sent by the server. 
+  //              You can then determine how to merge the subscription data into the existing state 
+  //              and return the updated data. All you’re doing inside updateQuery is retrieving the new link 
+  //              from the received subscriptionData, merging it into the existing list of links 
+  //              and returning the result of this operation.
+
+  _subscribeToNewLinks = subscribeToMore => {
+    subscribeToMore({
+      document:NEW_LINKS_SUBSCRIPTION,
+      updateQuery: (prev, { subscriptionData }) => {
+        if ( !subscriptionData.data) return prev
+        const newLink = subscriptionData.data.newLink
+        const exists = prev.feed.links.find(({id}) => id === newLink.id )
+        if ( exists ) return prev;
+
+        return Object.assign({}, prev, { feed : {
+            links: [newLink, ...prev.feed.links],
+            count: prev.feed.links.length + 1,
+            __tpename: prev.feed.__typename
+          }
+        })
+      }
+    })
+  }
+
+  _subscribeToNewVotes = subscribeToMore => {
+    subscribeToMore({
+      document: NEW_VOTES_SUBSCRIPTION
+    })
+  }
+
+  render() {
+    return (
       <Query query={FEED_QUERY}>
-        {({loading, error, data}) => {
+        {({loading, error, data, subscribeToMore}) => {
           if (loading) return <div>fetching...</div>
           if(error) return <div>Error...</div>
           
+          // the two subscriptions in our app for links
+          this._subscribeToNewLinks(subscribeToMore)
+          this._subscribeToNewVotes(subscribeToMore)
+
           const linksToRender = data.feed.links;
 
           return (        
@@ -50,7 +142,7 @@ const LinkList = () => {
                   <Link 
                     index={index} 
                     link={link} 
-                    updateStoreAfterVote={ _updateCacheAfterVote}/>
+                    updateStoreAfterVote={ this._updateCacheAfterVote}/>
                 </li>))
               }
             </ul>
@@ -58,6 +150,8 @@ const LinkList = () => {
         }}
       </Query>
     )
+  }
+
 }
 
 export default LinkList
--- src/index.js
+++ src/index.js
@@ -8,16 +8,48 @@ import { AUTH_TOKEN } from './constants'
 
 
 import { ApolloProvider } from 'react-apollo'
+import { split } from 'apollo-link'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { setContext } from 'apollo-link-context'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 
+
+
+// Here you create the httpLink that will connect your ApolloClient instance 
+// with the GraphQL API, your GraphQL server will be running on http://localhost:4000.
 
 const httpLink = createHttpLink({
     uri: 'http://localhost:4000'
 })
 
+
+// You’re instantiating a WebSocketLink that knows the subscriptions endpoint. 
+// The subscriptions endpoint in this case is similar to the HTTP endpoint, 
+// except that it uses the ws instead of http protocol. 
+// Notice that you’re also authenticating the websocket connection 
+// with the user’s token that you retrieve from localStorage.
+
+const wsLink = new WebSocketLink({
+    uri: `ws://localhost:4000`,
+    options: {
+        reconnect: true,
+        connectionParams: {
+            authToken: localStorage.getItem(AUTH_TOKEN)
+        }
+    }
+})
+
+
+// This middleware will be invoked every time ApolloClient sends a request to the server. 
+// Apollo Links allow you to create middlewares that let you modify requests 
+// before they are sent to the server.
+
+// Let’s see how it works in our code: 
+// first, we get the authentication token from localStorage if it exists; 
+// after that, we return the headers to the context so httpLink can read them.
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem(AUTH_TOKEN)
@@ -29,10 +61,42 @@ const authLink = setContext((_, { headers }) => {
   }
 })
 
+
+
+// split is used to “route” a request to a specific middleware link. 
+// It takes three arguments, the first one is a test function which returns a boolean. 
+// The remaining two arguments are again of type ApolloLink. 
+// If test returns true, the request will be forwarded to the link passed as the second argument. 
+// If false, to the third one.
+
+
+// In your case, the test function is checking whether the requested operation is a subscription. 
+// If this is the case, it will be forwarded to the wsLink, 
+// otherwise (if it’s a query or mutation), the authLink.concat(httpLink) will take care of it:
+
+const link = split(
+    ({ query }) => {
+        const { kind, operation } = getMainDefinition(query)
+        return kind === 'OperationDefinition' && operation === 'subscription'
+    },
+    wsLink,
+    authLink.concat(httpLink)
+)
+
+// Now you instantiate ApolloClient by passing in the httpLink 
+// and a new instance of an InMemoryCache.
+// Now you also need to make sure ApolloClient gets instantiated 
+// with the correct link - update the constructor call as follows:
+
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
+    link,
     cache: new InMemoryCache()
 })
+
+
+// Finally you render the root component of your React app. 
+// The App is wrapped with the higher-order component ApolloProvider 
+// that gets passed the client as a prop.
 
 ReactDOM.render(
     <BrowserRouter>
